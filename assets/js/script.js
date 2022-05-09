5cf3fc72777c27ccff5ebacd51d40ef442c35642
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 12:30:15 -0300

    script initial

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event){
+    if(event.keyCode === 32){
+        jump();
+    }
+}
+
+function jump() {  
+    let position = 0;
+    let upInterval = setInterval(() => {
+      if (position >= 150) {
+        clearInterval(upInterval);
+  
+        let downInterval = setInterval(() => {
+          if (position <= 0) {
+            clearInterval(downInterval);
+          } else {
+            position -= 20;
+            dino.style.bottom = position + 'px';
+          }
+        }, 20);
+      } else {
+        position += 20;
+        dino.style.bottom = position + 'px';
+      }
+    }, 20);
+  }
+
+document.addEventListener('keyup', handleKeyUp);
