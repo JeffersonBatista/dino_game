85102845c8b47ebbdba51999bb18bc27f6edade5
Jefferson_Batista <jefferson_tecnico@hotmail.com>
2022-05-09 12:46:20 -0300

    create cactus

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,44 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJumping = false;
 
 function handleKeyUp(event){
     if(event.keyCode === 32){
-        jump();
+        if(!isJumping){
+            jump();
+        }
     }
 }
 
 function jump() {  
     let position = 0;
+    isJumping = true;
     let upInterval = setInterval(() => {
       if (position >= 150) {
         clearInterval(upInterval);
-  
         let downInterval = setInterval(() => {
           if (position <= 0) {
+            isJumping = false;
             clearInterval(downInterval);
           } else {
             position -= 20;
             dino.style.bottom = position + 'px';
           }
-        }, 20);
+        }, 15);
       } else {
         position += 20;
         dino.style.bottom = position + 'px';
       }
-    }, 20);
+    }, 15);
   }
 
+function createCactus(){
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);
